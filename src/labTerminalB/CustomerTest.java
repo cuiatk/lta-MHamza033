dc7c90fecf734e5660dfe82c3dce3dbf2a7e51a0
Acer <Acer@DESKTOP-F4ER34H>
2019-06-13 03:36:21 -0700

    TODO 2 completed

--- src/labTerminalB/CustomerTest.java
+++ src/labTerminalB/CustomerTest.java
@@ -31,6 +31,38 @@ public class CustomerTest {
 	 * Provide at least two more test cases carefully chosen so that they satisfy our testing
 	 * strategy basics i.e. input partition etc
 	 */
-	
+	/**
+	 * Test 2 Customer rented two movies one REGULAR and one NEW_RELEASE
+	 */
+	@Test
+	public void testStatement2() {
+		me.addRental(r1);
+		me.addRental(r2);
+		//fail("Not yet implemented");
+		String output1 = "Rental Record for Nadir\r\n" + 
+				"	Spancer	33.5\r\n" + 
+				"	Speed	12.0";
+		if(output1.equals(me.statement()))
+		{
+			assert true;
+		}
+		
+		/**
+		 * Test 3 Customer rented Zero Movie
+		 */
+		@Test
+		public void testStatement3() {
+			
+			//fail("Not yet implemented");
+			String output1 = "Rental Record for Nadir\r\n" + 
+					"	Spancer	33.5\ r\n" + 
+					"	Speed	12.0";
+			if(output1.equals(me.statement()))
+			{
+				assert true;
+			}
+			
 
+
+		}
 }
